b6b78292c41ca6ab31f36f7dde889bdb20608025
Katsuya <haradakatsuya190511@gmail.com>
2026-07-16 20:22:50 -0700

    switch UI of header by session

--- components/Organisms/Header.tsx
+++ components/Organisms/Header.tsx
@@ -1,10 +1,20 @@
 'use client';
 
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import LogoutButton from '@/components/Atoms/LogoutButton';
+
 export default function Header() {
+  const { data: session } = useSession();
+
   return (
     <header className='bg-blue-500 w-full h-10'>
       <div className='flex justify-between max-w-screen-xl w-full h-full px-4 mx-auto'>
-        <h1 className='text-white text-3xl font-bold leading-10'>Chat Time</h1>
+        {session
+          ? <Link href='/' className='text-white text-3xl font-bold leading-10 duration-300 hover:opacity-60'>Chat Time</Link>
+          : <h1 className='text-white text-3xl font-bold leading-10'>Chat Time</h1>
+        }
+        {session && <LogoutButton />}
       </div>
     </header>
   );
