8e602c0c7b1a398b8d537363bdcf6f641c0012ba
Katsuya <haradakatsuya190511@gmail.com>
2026-07-31 19:40:54 +0700

    updated SearchUsers.tsx

--- components/Organisms/SearchUsers.tsx
+++ components/Organisms/SearchUsers.tsx
@@ -1,3 +1,62 @@
-export default function SearchUsers() {
-  return <div></div>;
+'use client';
+
+import { useState } from 'react';
+import type { Session, User } from 'next-auth';
+import { Input } from '@mui/material';
+import UserList from '@/components/Molecules/UserList';
+import { blue } from '@mui/material/colors';
+
+export default function SearchUsers(props: {session: Session, friendIds: number[]}) {
+  const [users, setUsers] = useState<User[]>([]);
+  const [friendIds, setFriendIds] = useState<number[]>(props.friendIds);
+
+  const searchUsers = async (name: string) => {
+    name = name.trim();
+    if (!name) {
+      setUsers([]);
+      return;
+    }
+    let users: User[] = [];
+    const res = await fetch(`/api/getUsersByName?name=${name}`);
+    if (res.ok) users = await res.json();
+    setUsers(users);
+  };
+
+  const handleFriendAdded = (newFriendId: number) => {
+    setFriendIds((prev) => [...prev, newFriendId]);
+  };
+
+  return (
+    <div className='w-full h-full py-20'>
+      <Input
+        disableUnderline
+        sx={{
+          display: 'block',
+          width: '100%',
+          maxWidth: '400px',
+          height: '50px',
+          padding: '0 20px',
+          margin: '0 auto'
+        }}
+        inputProps={{
+          sx: {
+            display: 'block',
+            border: '2px solid',
+            borderColor: blue,
+            boxSizing: 'border-box',
+            width: '100%',
+            height: '100%',
+            p: 1,
+            appearance: 'none'
+          }
+        }}
+        onChange={(e) => searchUsers(e.target.value)}
+      />
+      <ul className='block text-center py-10'>
+        {users.map(user => (
+            <UserList key={user.id} user={user} myId={props.session.user.id} friendIds={friendIds} onFriendAdded={handleFriendAdded} />
+        ))}
+      </ul>
+    </div>
+  );
 }
