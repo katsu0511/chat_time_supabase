ab52cda9328b824e08789ddd782854613f408cc6
Katsuya <haradakatsuya190511@gmail.com>
2026-08-04 20:40:03 +0700

    modified home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { auth } from '@/lib/getSession';
 import { redirect } from 'next/navigation';
+import { NextResponse } from 'next/server';
+import { getFriends } from '@/lib/getter';
 import Messages from '@/components/Organisms/Messages';
 
 export default async function Home() {
   const session = await auth();
   if (!session) return redirect('/login');
-  return <Messages session={session} />;
+  const id = Number(session.user.id);
+  if (isNaN(id)) return NextResponse.json({ error: 'ID must be a number' }, { status: 400 });
+  const friends = await getFriends(id);
+  return <Messages session={session} friends={friends} />;
 }
