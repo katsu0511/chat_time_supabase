dda576cd19563ec36594f29e17bd329d24a326eb
Katsuya <haradakatsuya190511@gmail.com>
2026-08-06 12:46:43 +0700

    updated home screen

--- components/Organisms/Messages.tsx
+++ components/Organisms/Messages.tsx
@@ -2,37 +2,21 @@
 
 import type { Session, User } from 'next-auth';
 import { useState, useCallback, useEffect, useRef } from 'react';
-// import MessageContent from './MessageContent';
-// import SendMessage from './SendMessage';
+import MessageContent from '@/components/Molecules/MessageContent';
+import SendMessage from '@/components/Molecules/SendMessage';
 
-export default function Messages({session}: {session?: Session}) {
-  const [friends, setFriends] = useState<User[]>([]);
+export default function Messages({session, friends}: {session: Session, friends: User[]}) {
   const [messages, setMessages] = useState<Message[]>([]);
   const [friendId, setFriendId] = useState<number>();
-  const [socket, setSocket] = useState<WebSocket | null>(null);
   const messageContainerRef = useRef<HTMLDivElement>(null);
 
-  // const getMessages = useCallback(async (friendId: number) => {
-  //   setFriendId(friendId);
-  //   const res = await fetch(`/api/getMessages?id=${session.user.id}&friendId=${friendId}`);
-  //   if (!res.ok) setMessages([]);
-  //   const contents: Message[] = await res.json();
-  //   setMessages(contents);
-  // }, [session.user.id]);
-
-  const getFriends = useCallback(async (userId: string) => {
-    const res = await fetch(`/api/getFriends?id=${userId}`);
-    if (!res.ok) {
-      setFriends([]);
-      return;
-    }
-    const friends: User[] = await res.json();
-    setFriends(friends);
-  }, []);
-
-  // useEffect(() => {
-    // getFriends(session.user.id);
-  // }, [session.user.id, getFriends]);
+  const getMessages = useCallback(async (friendId: number) => {
+    setFriendId(friendId);
+    const res = await fetch(`/api/getMessages?id=${session.user.id}&friendId=${friendId}`);
+    if (!res.ok) setMessages([]);
+    const contents: Message[] = await res.json();
+    setMessages(contents);
+  }, [session.user.id]);
 
   // useEffect(() => {
   //   if (!friendId) return;
@@ -68,7 +52,7 @@ export default function Messages({session}: {session?: Session}) {
             <li key={friend.id} className='w-full h-24'>
               <button
                 className={`w-full h-full p-2 cursor-pointer duration-300 ${friendId === Number(friend.id) && 'bg-blue-500 shadow-xl'} hover:bg-blue-500 hover:shadow-xl`}
-                // onClick={() => getMessages(Number(friend.id))}
+                onClick={() => getMessages(Number(friend.id))}
               >
                 <p className='w-full h-[50%] text-2xl leading-10 text-left'>{friend.name}</p>
                 <p className='w-full h-[50%] text-lg leading-10 text-left'>{friend.userId}</p>
@@ -80,15 +64,13 @@ export default function Messages({session}: {session?: Session}) {
       <div className='w-[70%] h-full'>
         <div ref={messageContainerRef} className='bg-blue-300 w-full h-[calc(100%-40px)] overflow-y-auto'>
           {messages.map(message => (
-            // <MessageContent key={message.messageId} id={Number(session.user.id)} message={message} />
-            <div key={message.messageId}></div>
+            <MessageContent key={message.messageId} id={Number(session.user.id)} message={message} />
           ))}
         </div>
         {
           friendId === undefined
           ? <div className='bg-blue-300 w-full h-10'></div>
-          // : <SendMessage senderId={Number(session.user.id)} receiverId={friendId} socket={socket} />
-          : <div></div>
+          : <SendMessage senderId={Number(session.user.id)} receiverId={friendId} />
         }
       </div>
     </div>
