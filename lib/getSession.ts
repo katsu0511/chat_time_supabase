cbd947f7dfba03c04a7dd96f45ea6a32d6407999
Katsuya <haradakatsuya190511@gmail.com>
2026-08-10 12:01:58 +0700

    modified a bit

--- lib/getSession.ts
+++ lib/getSession.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { getUserForLogin, getUser } from '@/lib/getter';
 import { compare } from 'bcrypt';
