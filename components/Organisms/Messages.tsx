f5ba7393b9b0b65701ab5ed63e0613c7406c8cf4
Katsuya <haradakatsuya190511@gmail.com>
2026-08-15 14:42:56 +0700

    modified css

--- components/Organisms/Messages.tsx
+++ components/Organisms/Messages.tsx
@@ -26,13 +26,13 @@ export default function Messages({session, friends}: {session: Session, friends:
   }, [messages]);
 
   return (
-    <div className='flex w-full h-full md:border-blue-500 md:border-x-4'>
+    <div className='flex w-full h-full md:border-[color:var(--color-primary)] md:border-x-4'>
       <div className='w-[30%] h-full'>
         <ul>
           {friends.map(friend => (
             <li key={friend.id} className='w-full h-24'>
               <button
-                className={`w-full h-full p-2 cursor-pointer duration-300 ${friendId === Number(friend.id) && 'bg-blue-500 shadow-xl'} hover:bg-blue-500 hover:shadow-xl`}
+                className={`w-full h-full p-2 cursor-pointer duration-300 ${friendId === Number(friend.id) && 'bg-[color:var(--color-secondary)] shadow-xl'} hover:bg-[color:var(--color-secondary)] hover:shadow-xl`}
                 onClick={() => getMessages(Number(friend.id))}
               >
                 <p className='w-full h-[50%] text-2xl leading-10 text-left'>{friend.name}</p>
@@ -43,14 +43,14 @@ export default function Messages({session, friends}: {session: Session, friends:
         </ul>
       </div>
       <div className='w-[70%] h-full'>
-        <div ref={messageContainerRef} className='bg-blue-300 w-full h-[calc(100%-40px)] overflow-y-auto'>
+        <div ref={messageContainerRef} className='bg-[color:var(--light-secondary)] w-full h-[calc(100%-40px)] overflow-y-auto'>
           {messages.map(message => (
             <MessageContent key={message.messageId} id={Number(session.user.id)} message={message} />
           ))}
         </div>
         {
           friendId === undefined
-          ? <div className='bg-blue-300 w-full h-10'></div>
+          ? <div className='bg-[color:var(--light-secondary)] w-full h-10'></div>
           : <SendMessage senderId={Number(session.user.id)} receiverId={friendId} />
         }
       </div>
