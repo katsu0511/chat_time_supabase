2d56e0f44bfcbd1810777f2e38ea6a13c501b730
Katsuya <haradakatsuya190511@gmail.com>
2026-08-21 15:50:14 +0700

    modified a bit

--- components/Organisms/Header.tsx
+++ components/Organisms/Header.tsx
@@ -1,20 +1,18 @@
-'use client';
-
-import { useSession } from 'next-auth/react';
+import getAuthUser from '@/lib/getAuthUser';
 import Link from 'next/link';
 import LogoutButton from '@/components/Atoms/LogoutButton';
 
-export default function Header() {
-  const { data: session } = useSession();
+export default async function Header() {
+  const user = await getAuthUser();
 
   return (
     <header className='bg-[color:var(--color-primary)] w-full h-10'>
       <div className='flex justify-between max-w-screen-xl w-full h-full px-4 mx-auto'>
-        {session
+        {user
           ? <Link href='/' className='text-white text-3xl font-bold leading-10 duration-300 hover:opacity-60'>Chat Time</Link>
           : <h1 className='text-white text-3xl font-bold leading-10'>Chat Time</h1>
         }
-        {session && <LogoutButton />}
+        {user && <LogoutButton />}
       </div>
     </header>
   );
