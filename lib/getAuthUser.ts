67cad6211d1c67cd82e7b2d4e807b02066fabdfa
Katsuya <haradakatsuya190511@gmail.com>
2026-08-21 15:47:59 +0700

    change file name
    changed file name getSession.ts to getAuthUser.ts

---
+++ lib/getAuthUser.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@/lib/supabaseServer';
+
+export async function getSession() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data.user) {
+    return null;
+  }
+  return data.user;
+}
