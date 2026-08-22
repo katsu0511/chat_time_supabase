288e1f763daff526e1765289698618f80c724640
Katsuya <haradakatsuya190511@gmail.com>
2026-08-22 21:04:52 +0700

    deleted SessionProviderWrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import '@/app/globals.css';
 import { Inconsolata } from 'next/font/google';
 import type { Metadata } from 'next';
-import { auth } from '@/lib/getSession';
-import SessionProviderWrapper from '@/components/Templates/SessionProviderWrapper';
+import type { User as AuthUser } from '@supabase/supabase-js';
+import getAuthUser from '@/lib/getAuthUser';
 import { ThemeProviderWrapper } from '@/components/Templates/ThemeProviderWrapper';
 import Header from '@/components/Organisms/Header';
 import Main from '@/components/Templates/Main';
@@ -20,18 +20,16 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
+  const user: AuthUser | null = await getAuthUser();
 
   return (
     <html lang='en' className='select-none'>
       <body className={fnt.className}>
-        <SessionProviderWrapper session={session}>
-          <ThemeProviderWrapper>
-            <Header />
-            <Main>{children}</Main>
-            <Footer />
-          </ThemeProviderWrapper>
-        </SessionProviderWrapper>
+        <ThemeProviderWrapper>
+          <Header user={user} />
+          <Main>{children}</Main>
+          <Footer user={user} />
+        </ThemeProviderWrapper>
       </body>
     </html>
   );
