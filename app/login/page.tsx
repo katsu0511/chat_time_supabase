a193e3982e66a9e976a5a5156e94495cc8dcb5a0
Katsuya <haradakatsuya190511@gmail.com>
2026-08-22 17:58:38 +0700

    modified a bit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,11 @@
-import { auth } from '@/lib/getSession';
+import getAuthUser from '@/lib/getAuthUser';
 import { redirect } from 'next/navigation';
 import LoginForm from '@/components/Organisms/LoginForm';
 
 export default async function Login() {
-  const session = await auth();
-  if (session) return redirect('/');
+  const user = await getAuthUser();
+  if (user) {
+    redirect('/');
+  }
   return <LoginForm />;
 }
