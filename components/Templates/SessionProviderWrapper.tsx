3e3ce22052c7700078e7501d3de327da56e464b1
Katsuya <haradakatsuya190511@gmail.com>
2026-08-23 11:26:26 +0700

    deleted SessionProviderWrapper.tsx

--- components/Templates/SessionProviderWrapper.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { Session } from 'next-auth';
-import { SessionProvider } from 'next-auth/react';
-
-export default function SessionProviderWrapper({ children, session }: { children: React.ReactNode, session: Session | null }) {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
-}
