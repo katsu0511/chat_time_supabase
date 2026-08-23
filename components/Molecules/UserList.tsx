3e1450530b354ad2fbb8aefefc29f76d5b1006ee
Katsuya <haradakatsuya190511@gmail.com>
2026-08-23 11:29:33 +0700

    modified a bit

--- components/Molecules/UserList.tsx
+++ components/Molecules/UserList.tsx
@@ -1,11 +1,10 @@
-import type { User } from 'next-auth';
 import { addFriend } from '@/lib/actions';
 import { Button } from '@mui/material';
 
-export default function UserList(props: {user: User, myId: string, friendIds: number[], onFriendAdded: (id: number) => void}) {
+export default function UserList(props: {user: AppUser, myId: string, friendIds: string[], onFriendAdded: (id: string) => void}) {
   const handleAddFriend = async () => {
-    const result = await addFriend(Number(props.myId), Number(props.user.id));
-    if (result && props.onFriendAdded) props.onFriendAdded(Number(props.user.id));
+    const result = await addFriend(props.myId, props.user.id);
+    if (result && props.onFriendAdded) props.onFriendAdded(props.user.id);
   };
 
   return (
@@ -13,7 +12,7 @@ export default function UserList(props: {user: User, myId: string, friendIds: nu
       <div className='flex w-full h-full border-[color:var(--color-primary)] border-b-1'>
         <div className='w-[calc(100%-72px)] max-w-82 h-full text-left px-2'>
           <p className='text-xl w-full h-[25px]'>{props.user.name}</p>
-          <p className='text-sm w-full h-6 leading-6'>{props.user.userId}</p>
+          <p className='text-sm w-full h-6 leading-6'>{props.user.email}</p>
         </div>
         <div className='w-18 h-full pr-2'>
           {
@@ -22,7 +21,7 @@ export default function UserList(props: {user: User, myId: string, friendIds: nu
               variant='contained'
               color='secondary'
               disableElevation={true}
-              disabled={props.friendIds.includes(props.user.id as unknown as number)}
+              disabled={props.friendIds.includes(props.user.id)}
               onClick={handleAddFriend}
               sx={{
                 display: 'block',
@@ -35,7 +34,7 @@ export default function UserList(props: {user: User, myId: string, friendIds: nu
                 marginBottom: '6px'
               }}
             >
-              {props.friendIds.includes(props.user.id as unknown as number) ? 'Friend' : 'Add'}
+              {props.friendIds.includes(props.user.id) ? 'Friend' : 'Add'}
             </Button>
           }
         </div>
