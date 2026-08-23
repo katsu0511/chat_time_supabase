441c29ca0c641f46931febf3c1cbf996f2fb8137
Katsuya <haradakatsuya190511@gmail.com>
2026-08-23 11:25:50 +0700

    modified API

--- app/api/getMessages/route.ts
+++ app/api/getMessages/route.ts
@@ -3,14 +3,10 @@ import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
-  const idParam = searchParams.get('id');
-  const friendIdParam = searchParams.get('friendId');
-  if (!idParam) return NextResponse.json({ error: 'ID is required' }, { status: 400 });
-  if (!friendIdParam) return NextResponse.json({ error: 'Friend ID is required' }, { status: 400 });
-  const id = Number(idParam);
-  if (isNaN(id)) return NextResponse.json({ error: 'ID must be a number' }, { status: 400 });
-  const friendId = Number(friendIdParam);
-  if (isNaN(friendId)) return NextResponse.json({ error: 'Friend ID must be a number' }, { status: 400 });
-  const messages = await getMessages(id, friendId);
+  const userId = searchParams.get('userId');
+  const friendId = searchParams.get('friendId');
+  if (!userId) return NextResponse.json({ error: 'User ID is required' }, { status: 400 });
+  if (!friendId) return NextResponse.json({ error: 'Friend ID is required' }, { status: 400 });
+  const messages = await getMessages(userId, friendId);
   return NextResponse.json(messages);
 }
