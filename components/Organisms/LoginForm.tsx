023315c1fcf2b7fed61f9ae9572b5c40107d5d2b
Katsuya <haradakatsuya190511@gmail.com>
2026-08-24 11:05:24 +0700

    modified a bit

--- components/Organisms/LoginForm.tsx
+++ components/Organisms/LoginForm.tsx
@@ -7,17 +7,17 @@ import Button from '@/components/Molecules/Button';
 import PageLink from '@/components/Atoms/PageLink';
 
 export default function LoginForm() {
-  const { userid, setUserid, password, setPassword, error, setError, router } = useAuth();
+  const { email, setEmail, password, setPassword, error, setError, router } = useAuth();
 
   const login = async (e: React.SubmitEvent) => {
     e.preventDefault();
-    handleLogin(userid, password, router, setError);
+    handleLogin(email, password, router, setError);
   };
 
   return (
     <div className='flex items-center w-full h-full'>
       <form className='w-full' onSubmit={login}>
-        <Input label='User ID' type='text' value={userid} onChange={(e) => setUserid(e.target.value)} />
+        <Input label='Email' type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
         <Input label='Password' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
         <Button usage='Login' error={error} />
         <PageLink usage='signup' />
