6ec93b1f722145a2fc440fea11fac80185df4d3b
Katsuya <haradakatsuya190511@gmail.com>
2026-08-26 11:59:04 +0700

    modified a bit

--- components/Organisms/Header.tsx
+++ components/Organisms/Header.tsx
@@ -1,8 +1,8 @@
-import type { User as AuthUser } from '@supabase/supabase-js';
+import type { User } from '@supabase/supabase-js';
 import Link from 'next/link';
 import LogoutButton from '@/components/Atoms/LogoutButton';
 
-export default async function Header({user}: {user: AuthUser | null}) {
+export default async function Header({user}: {user: User | null}) {
   return (
     <header className='bg-[color:var(--color-primary)] w-full h-10'>
       <div className='flex justify-between max-w-screen-xl w-full h-full px-4 mx-auto'>
