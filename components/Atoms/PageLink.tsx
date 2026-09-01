b98992ccd06bc2d010fb874b502810de3bc06946
Katsuya <haradakatsuya190511@gmail.com>
2026-09-01 22:36:15 +0700

    modified css

--- components/Atoms/PageLink.tsx
+++ components/Atoms/PageLink.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function PageLink({ path, display }: { path: string, display: string }) {
   return (
-    <div className='flex justify-center pt-10'>
+    <div className='flex justify-center mt-10'>
       <Link href={`./${path}`} className='inline-block text-[color:var(--color-primary)] font-bold duration-300 hover:opacity-40 cursor-pointer'>&gt;&gt; {display}</Link>
     </div>
   );
