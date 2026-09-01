2dff8b7ccbbf03751b3419747fc6ebe93601053e
Katsuya <haradakatsuya190511@gmail.com>
2026-09-01 22:35:45 +0700

    modified css

--- components/Atoms/Heading.tsx
+++ components/Atoms/Heading.tsx
@@ -1,3 +1,3 @@
 export default function Heading(props: {title: string}) {
-  return <h2 className='text-2xl font-bold text-[color:var(--color-primary)] text-center'>{props.title}</h2>;
+  return <h2 className='text-2xl font-bold text-[color:var(--color-primary)] text-center mb-10'>{props.title}</h2>;
 }
