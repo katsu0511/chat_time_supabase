bd8ac7ea64914ff76b4ed3827299b66080e224b6
Katsuya <haradakatsuya190511@gmail.com>
2026-09-01 22:02:07 +0700

    added Heading and PageLink to theme setting page

--- app/setting/theme/page.tsx
+++ app/setting/theme/page.tsx
@@ -2,8 +2,10 @@ import type { User as AuthUser } from '@supabase/supabase-js';
 import getAuthUser from '@/lib/auth/getAuthUser';
 import { redirect } from 'next/navigation';
 import { FormControl } from '@mui/material';
+import Heading from '@/components/Atoms/Heading';
 import ThemeModeWrapper from '@/components/Organisms/ThemeModeWrapper';
 import ThemeColorWrapper from '@/components/Organisms/ThemeColorWrapper';
+import PageLink from '@/components/Atoms/PageLink';
 
 export default async function ThemeSetting() {
   const user: AuthUser | null = await getAuthUser();
@@ -20,8 +22,10 @@ export default async function ThemeSetting() {
           height: '100%'
         }}
       >
+        <Heading title='Theme Setting' />
         <ThemeModeWrapper />
         <ThemeColorWrapper />
+        <PageLink path='' display='Setting' />
       </FormControl>
     </div>
   );
