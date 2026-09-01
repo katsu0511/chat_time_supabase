ecbca25566828b72484265fae3aa59ff8fa2c07c
Katsuya <haradakatsuya190511@gmail.com>
2026-09-01 22:16:39 +0700

    modified a bit

--- components/Organisms/Footer.tsx
+++ components/Organisms/Footer.tsx
@@ -6,9 +6,9 @@ export default async function Footer({user}: {user: AuthUser | null}) {
     <footer className='bg-[color:var(--color-primary)] w-full h-10'>
       {user
         ? <div className='flex justify-center max-w-screen-xl w-full h-full px-4 mx-auto'>
-            <FooterLink display='Chat' url='/'/>
-            <FooterLink display='Friend' url='/friend'/>
-            <FooterLink display='Setting' url='/setting'/>
+            <FooterLink display='Chat' url=''/>
+            <FooterLink display='Friend' url='friend'/>
+            <FooterLink display='Setting' url='setting'/>
           </div>
         : <div className='text-white text-xl max-w-screen-xl w-full h-full leading-10 text-center px-4 mx-auto'>&copy;Chat time</div>
       }
