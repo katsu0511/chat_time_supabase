c324e16419388fa2e74620678a78d295c579ffe5
Katsuya <haradakatsuya190511@gmail.com>
2026-09-02 11:21:09 +0700

    added value props

--- components/Molecules/LanguageSelect.tsx
+++ components/Molecules/LanguageSelect.tsx
@@ -2,12 +2,13 @@ import { ChangeEvent } from 'react';
 import Option from '@/components/Atoms/Option';
 import { Languages, Language } from '@/lib/domain/languages';
 
-export default function LanguageSelect(props: {label: string, onChange: (e: ChangeEvent<HTMLSelectElement>) => void}) {
+export default function LanguageSelect(props: {label: string, value: Language, onChange: (e: ChangeEvent<HTMLSelectElement>) => void}) {
   return (
     <div className='flex justify-center items-center py-2'>
       <label htmlFor={props.label} className='block w-20'>{props.label}: </label>
       <select
         id={props.label}
+        value={props.value}
         className='border-[color:var(--color-primary)] w-47 h-9 border-2 px-2 py-1 outline-none appearance-none'
         onChange={props.onChange}
         required
