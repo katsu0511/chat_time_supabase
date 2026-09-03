48bc6488821a4aeeadbf4f686b093f4b09b6c81c
Katsuya <haradakatsuya190511@gmail.com>
2026-09-03 16:55:21 +0700

    modified css

--- components/Templates/Main.tsx
+++ components/Templates/Main.tsx
@@ -1,7 +1,7 @@
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
-    <main className='w-full h-[calc(100dvh-80px)]'>
-      <div className='w-full h-full md:max-w-screen-md xl:max-w-screen-xl md:px-4 md:mx-auto'>
+    <main className='flex w-full min-h-[calc(100dvh-80px)] py-20'>
+      <div className='w-full flex-1 md:max-w-screen-md xl:max-w-screen-xl md:px-4 md:mx-auto'>
         {children}
       </div>
     </main>
