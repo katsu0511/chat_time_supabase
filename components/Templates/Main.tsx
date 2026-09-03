79f66c73b320aea9206b2983599e14022b4c813d
Katsuya <haradakatsuya190511@gmail.com>
2026-09-03 16:56:45 +0700

    modified css by pathname

--- components/Templates/Main.tsx
+++ components/Templates/Main.tsx
@@ -1,6 +1,12 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
 export default function Main({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
   return (
-    <main className='flex w-full min-h-[calc(100dvh-80px)] py-20'>
+    <main className={`flex w-full min-h-dvh ${pathname === '/' ? 'py-10' : 'py-20'}`}>
       <div className='w-full flex-1 md:max-w-screen-md xl:max-w-screen-xl md:px-4 md:mx-auto'>
         {children}
       </div>
