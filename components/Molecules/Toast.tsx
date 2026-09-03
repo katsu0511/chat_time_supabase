ac6d6852f5456374bd6205ae36c2ec35db3853cb
Katsuya <haradakatsuya190511@gmail.com>
2026-09-03 16:12:51 +0700

    modified Toast.tsx to be used for multiple purposes

--- components/Molecules/Toast.tsx
+++ components/Molecules/Toast.tsx
@@ -4,30 +4,31 @@ import { Dispatch, SetStateAction } from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Snackbar, Alert } from '@mui/material';
 
 type Props = {
+  type: string
   dialogOpen: boolean
   setDialogOpen: Dispatch<SetStateAction<boolean>>
   snackbarOpen: boolean
   setSnackbarOpen: Dispatch<SetStateAction<boolean>>
   handleCancel: () => void
-  changeEmail: () => Promise<void>
+  changeAuthInfo: () => Promise<void>
 };
 
-export default function Toast({ dialogOpen, setDialogOpen, snackbarOpen, setSnackbarOpen, handleCancel, changeEmail }: Props) {
+export default function Toast({ type, dialogOpen, setDialogOpen, snackbarOpen, setSnackbarOpen, handleCancel, changeAuthInfo }: Props) {
   return (
     <>
       <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
-        <DialogTitle>Change your email</DialogTitle>
-        <DialogContent>Are you sure to change?
+        <DialogTitle>Change your {type}</DialogTitle>
+        <DialogContent>Are you sure to change your {type}?
           <br /><strong>This operation cannot be reverted.</strong>
           <br />After this operation, you will be logged out automatically.
         </DialogContent>
         <DialogActions>
           <Button color='error' onClick={() => handleCancel()}>Cancel</Button>
-          <Button color='success' onClick={() => changeEmail()}>Change</Button>
+          <Button color='success' onClick={() => changeAuthInfo()}>Change</Button>
         </DialogActions>
       </Dialog>
       <Snackbar open={snackbarOpen} autoHideDuration={2000} onClose={() => setSnackbarOpen(false)}>
-        <Alert severity='success' variant='filled'>Successfully changed your email</Alert>
+        <Alert severity='success' variant='filled'>Successfully changed your {type}</Alert>
       </Snackbar>
     </>
   );
