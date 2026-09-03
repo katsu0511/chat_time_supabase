333419fe358337f7eff599be3208bd9aca79ee05
Katsuya <haradakatsuya190511@gmail.com>
2026-09-03 18:12:45 +0700

    added overflow css

--- components/Organisms/ChatScreen.tsx
+++ components/Organisms/ChatScreen.tsx
@@ -13,7 +13,7 @@ export default function ChatScreen(
   }) {
   const messageContainerRef = useRef<HTMLDivElement>(null);
   const displayChatScreen = friendId === undefined ? 'hidden' : 'block';
-  const heightOfMessageContent = window.innerWidth < 768 ? 'h-[calc(100%-80px)]' : 'h-[calc(100%-40px)]';
+  const heightOfMessageContent = window.innerWidth < 768 ? 'h-[calc(100dvh-160px)]' : 'h-[calc(100dvh-120px)]';
 
   useEffect(() => {
     messageContainerRef.current?.scrollTo({
