962109cb2bf6b9cad680b8ef64cb5e67494643e3
Katsuya <haradakatsuya190511@gmail.com>
2026-09-03 12:27:29 +0700

    created Toast for changing email

---
+++ components/Molecules/Toast.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Dispatch, SetStateAction } from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Snackbar, Alert } from '@mui/material';
+
+type Props = {
+  dialogOpen: boolean
+  setDialogOpen: Dispatch<SetStateAction<boolean>>
+  snackbarOpen: boolean
+  setSnackbarOpen: Dispatch<SetStateAction<boolean>>
+  handleCancel: () => void
+  changeEmail: () => Promise<void>
+};
+
+export default function Toast({ dialogOpen, setDialogOpen, snackbarOpen, setSnackbarOpen, handleCancel, changeEmail }: Props) {
+  return (
+    <>
+      <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+        <DialogTitle>Change your email</DialogTitle>
+        <DialogContent>Are you sure to change?
+          <br /><strong>This operation cannot be reverted.</strong>
+          <br />After this operation, you will be logged out automatically.
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => handleCancel()}>Cancel</Button>
+          <Button color='error' onClick={() => changeEmail()}>Change</Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar open={snackbarOpen} autoHideDuration={2000} onClose={() => setSnackbarOpen(false)}>
+        <Alert severity='success' variant='filled'>Successfully changed your email</Alert>
+      </Snackbar>
+    </>
+  );
+}
