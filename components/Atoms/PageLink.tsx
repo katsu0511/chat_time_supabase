2b3e874b6b30a1f9fcc2e7780ffb2103a847719d
Katsuya <haradakatsuya190511@gmail.com>
2026-09-04 15:30:25 +0700

    switching UI by loading state

--- components/Atoms/PageLink.tsx
+++ components/Atoms/PageLink.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link';
 
-export default function PageLink({ path, display }: { path: string, display: string }) {
+export default function PageLink({ path, display, disabled = false }: { path: string, display: string, disabled?: boolean }) {
   return (
     <div className='flex justify-center mt-10'>
-      <Link href={`./${path}`} className='inline-block text-[color:var(--color-primary)] font-bold duration-300 hover:opacity-40 cursor-pointer'>&gt;&gt; {display}</Link>
+      {
+        disabled
+        ? <span className='inline-block text-gray-400 font-bold duration-300 cursor-not-allowed'>&gt;&gt; {display}</span>
+        : <Link href={`./${path}`} className='inline-block text-[color:var(--color-primary)] font-bold duration-300 cursor-pointer hover:opacity-40'>&gt;&gt; {display}</Link>
+      }
     </div>
   );
 }
--- components/Molecules/Button.tsx
+++ components/Molecules/Button.tsx
@@ -1,8 +1,12 @@
-export default function Button(props: {usage: string, error: string}) {
+export default function Button({ usage, error, disabled }: { usage: string, error: string, disabled: boolean }) {
+  const buttonClassName = disabled
+    ? 'bg-gray-400 text-white border-gray-400 hover:bg-gray-400 cursor-progress'
+    : 'bg-[color:var(--color-background)] text-[color:var(--color-primary)] border-[color:var(--color-primary)] hover:bg-[color:var(--color-primary)] hover:text-white cursor-pointer';
+
   return (
     <div className='py-2'>
-      {props.error && <p className='text-red-500 font-bold text-center mb-4'>{props.error}</p>}
-      <input type='submit' value={props.usage} className='block bg-[color:var(--color-background)] text-[color:var(--color-primary)] border-[color:var(--color-primary)] border-2 rounded-md h-10 w-20 mx-auto my-0 duration-300 hover:bg-[color:var(--color-primary)] hover:text-white cursor-pointer' />
+      {error && <p className='text-red-500 font-bold text-center mb-4'>{error}</p>}
+      <input type='submit' value={usage} disabled={disabled} className={`block border-2 rounded-md h-10 w-20 mx-auto my-0 duration-300 ${buttonClassName}`} />
     </div>
   );
 }
