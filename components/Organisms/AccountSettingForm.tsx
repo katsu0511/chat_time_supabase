f441408b73fde8645020b01bf22cfb4e163ad5da
Katsuya <haradakatsuya190511@gmail.com>
2026-09-04 18:15:46 +0700

    added loading state and modified change account setting execution

--- components/Organisms/AccountSettingForm.tsx
+++ components/Organisms/AccountSettingForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import useAuth from '@/lib/hooks/useAuth';
-import { useEffect } from 'react';
 import Heading from '@/components/Atoms/Heading';
 import Input from '@/components/Molecules/Input';
 import LanguageSelect from '@/components/Molecules/LanguageSelect';
@@ -10,12 +10,17 @@ import Button from '@/components/Molecules/Button';
 import PageLink from '@/components/Atoms/PageLink';
 
 export default function AccountSettingForm({ user }: { user: AppUser }) {
+  const [loading, setLoading] = useState(false);
   const { name, setName, language, setLanguage, error, setError, router } = useAuth();
 
   const changeSetting = async(e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
+    setError('');
+    
     if (user.name === name && user.language === language) {
       setError('You don\'t change any account setting');
+      setLoading(false);
       return;
     }
 
@@ -30,6 +35,7 @@ export default function AccountSettingForm({ user }: { user: AppUser }) {
     if (!res.ok) {
       const data = await res.json();
       setError(data.error);
+      setLoading(false);
       return;
     }
 
@@ -45,12 +51,12 @@ export default function AccountSettingForm({ user }: { user: AppUser }) {
     <div className='flex items-center w-full h-full'>
       <form className='w-full' onSubmit={changeSetting}>
         <Heading title='Account Setting' />
-        <Input label='Name' type='text' value={name} onChange={(e) => setName(e.target.value)}/>
-        <LanguageSelect label='Language' value={language} onChange={(e) => setLanguage(e.target.value as Language)} />
-        <Button usage='Change' error={error} />
-        <PageLink path='account/email' display='Email Setting' />
-        <PageLink path='account/password' display='Password Setting' />
-        <PageLink path='' display='Setting' />
+        <Input label='Name' type='text' value={name} disabled={loading} onChange={(e) => setName(e.target.value)}/>
+        <LanguageSelect label='Language' value={language} disabled={loading} onChange={(e) => setLanguage(e.target.value as Language)} />
+        <Button usage='Change' error={error} disabled={loading} />
+        <PageLink path='account/email' display='Email Setting' disabled={loading} />
+        <PageLink path='account/password' display='Password Setting' disabled={loading} />
+        <PageLink path='' display='Setting' disabled={loading} />
       </form>
     </div>
   );
