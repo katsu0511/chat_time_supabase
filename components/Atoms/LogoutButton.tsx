b794491fed0838a661e0eef50e8e34708c923486
Katsuya <haradakatsuya190511@gmail.com>
2026-09-05 11:20:54 +0700

    added loading state and modified logout execution

--- components/Atoms/LogoutButton.tsx
+++ components/Atoms/LogoutButton.tsx
@@ -1,17 +1,31 @@
 'use client';
 
+import { useState } from 'react';
 import useAuth from '@/lib/hooks/useAuth';
 import { handleLogout } from '@/lib/api/auth';
 
 export default function LogoutButton() {
+  const [loading, setLoading] = useState(false);
   const { router } = useAuth();
+
+  const buttonClassName = loading ? 'bg-gray-400 cursor-progress' : 'bg-[color:var(--color-primary)] cursor-pointer hover:bg-white hover:text-[color:var(--color-primary)]';
+
+  const logout = async () => {
+    setLoading(true);
+    const error = await handleLogout(router);
+    if (error) {
+      alert(error.message);
+      setLoading(false);
+    }
+  };
 
   return (
     <button
-      className='block bg-[color:var(--color-primary)] text-white h-10 w-20 duration-300 hover:bg-white hover:text-[color:var(--color-primary)] cursor-pointer'
-      onClick={() => handleLogout(router)}
+      disabled={loading}
+      className={`block text-white h-10 w-25 duration-300 ${buttonClassName}`}
+      onClick={() => logout()}
     >
-      Logout
+      {loading ? 'Loading..' : 'Logout'}
     </button>
   );
 }
