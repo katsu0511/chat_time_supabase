b06d5383da07dd7b503ad846273ffeb28138e715
Katsuya <haradakatsuya190511@gmail.com>
2026-09-05 11:04:55 +0700

    modified and refactored handleLogin

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -4,11 +4,7 @@ import { Language } from '@/lib/domain/languages';
 import { createUser } from '@/lib/api/actions';
 import { Prisma } from '@/lib/generated/prisma/client';
 
-export const handleLogin = async (
-  email: string,
-  password: string,
-  router: AppRouterInstance
-) => {
+export const handleLogin = async (email: string, password: string, router: AppRouterInstance) => {
   const { error } = await supabase.auth.signInWithPassword({ email, password });
   if (error) return { message: 'Failed to login' };
   router.push('/');
