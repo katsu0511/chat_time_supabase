98ffff822dc3af2c32ca1239c98a435df7bc7a90
Katsuya <haradakatsuya190511@gmail.com>
2026-09-06 15:35:35 +0700

    added loading state

--- components/Molecules/UserList.tsx
+++ components/Molecules/UserList.tsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
 import { Button } from '@mui/material';
 
 export default function UserList(props: {user: AppUser, myId: string, friendIds: string[], onFriendAdded: (id: string) => void}) {
+  const [loading, setLoading] = useState(false);
+
   const addFriend = async () => {
+    setLoading(true);
+
     const res = await fetch('/api/addFriend', {
       method: 'POST',
       headers: {
@@ -16,12 +21,14 @@ export default function UserList(props: {user: AppUser, myId: string, friendIds:
       const data = await res.json();
       alert(data.error);
     }
+
+    setLoading(false);
   };
 
   return (
     <li key={props.user.id} className='w-full max-w-100 h-[50px] mx-auto px-5 my-0'>
       <div className='flex w-full h-full border-[color:var(--color-primary)] border-b-1'>
-        <div className='w-[calc(100%-72px)] max-w-82 h-full text-left px-2'>
+        <div className='w-[calc(100%-84px)] max-w-82 h-full text-left px-2'>
           <p className='text-xl w-full h-[25px]'>{props.user.name}</p>
           <p className='text-sm w-full h-6 leading-6'>{props.user.email}</p>
         </div>
@@ -32,12 +39,12 @@ export default function UserList(props: {user: AppUser, myId: string, friendIds:
               variant='contained'
               color='secondary'
               disableElevation={true}
-              disabled={props.friendIds.includes(props.user.id)}
+              disabled={loading || props.friendIds.includes(props.user.id)}
               onClick={addFriend}
               sx={{
                 display: 'block',
                 color: 'white',
-                width: '64px',
+                width: '76px',
                 height: '36px',
                 borderRadius: '5px',
                 padding: 0,
@@ -45,7 +52,7 @@ export default function UserList(props: {user: AppUser, myId: string, friendIds:
                 marginBottom: '6px'
               }}
             >
-              {props.friendIds.includes(props.user.id) ? 'Friend' : 'Add'}
+              {loading ? 'Loading..' : props.friendIds.includes(props.user.id) ? 'Friend' : 'Add'}
             </Button>
           }
         </div>
