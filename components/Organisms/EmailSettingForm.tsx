27afae771e048e6a69715eba4f8dad3e5743d964
Katsuya <haradakatsuya190511@gmail.com>
2026-09-06 11:27:46 +0700

    modified a bit

--- components/Organisms/EmailSettingForm.tsx
+++ components/Organisms/EmailSettingForm.tsx
@@ -51,11 +51,14 @@ export default function EmailSettingForm({ user }: { user: AppUser }) {
     if (res.ok) {
       setSnackbarOpen(true);
       await new Promise((resolve) => setTimeout(resolve, 2000));
-      const error = await handleLogout(router);
+      const error = await handleLogout();
       if (error) {
         alert(error.message);
         setLoading(false);
+        return;
       }
+      router.push('/');
+      router.refresh();
     } else {
       const data = await res.json();
       setError(data.error);
