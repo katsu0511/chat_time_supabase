b8cf5c9431ac97342e4a6a4c10059c7ceb5e41e5
Katsuya <haradakatsuya190511@gmail.com>
2026-09-06 11:15:56 +0700

    modified css

--- components/Organisms/Header.tsx
+++ components/Organisms/Header.tsx
@@ -4,7 +4,7 @@ import LogoutButton from '@/components/Atoms/LogoutButton';
 
 export default function Header({user}: {user: AuthUser | null}) {
   return (
-    <header className='fixed bg-[color:var(--color-primary)] w-full h-10 z-10'>
+    <header className='fixed bg-[color:var(--color-primary)] w-full h-10 z-20'>
       <div className='flex justify-between max-w-screen-xl w-full h-full px-4 mx-auto'>
         {user
           ? <Link href='/' className='text-white text-3xl font-bold leading-10 duration-300 hover:opacity-60'>Chat Time</Link>
