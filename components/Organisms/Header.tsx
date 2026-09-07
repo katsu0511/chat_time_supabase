24fb49ef335a7bbd8f8d2ee0b5b686041edfb56f
Katsuya <haradakatsuya190511@gmail.com>
2026-09-07 16:49:21 +0700

    switching UI by hasClicked state

--- components/Organisms/Header.tsx
+++ components/Organisms/Header.tsx
@@ -1,14 +1,20 @@
+'use client';
+
 import type { User as AuthUser } from '@supabase/supabase-js';
+import useLoading from '@/lib/hooks/useLoading';
 import Link from 'next/link';
 import LogoutButton from '@/components/Atoms/LogoutButton';
 
-export default function Header({user}: {user: AuthUser | null}) {
+export default function Header({ user }: { user: AuthUser | null }) {
+  const { loading } = useLoading();
+
   return (
     <header className='fixed bg-[color:var(--color-primary)] w-full h-10 z-20'>
       <div className='flex justify-between max-w-screen-xl w-full h-full px-4 mx-auto'>
-        {user
-          ? <Link href='/' className='text-white text-3xl font-bold leading-10 duration-300 hover:opacity-60'>Chat Time</Link>
-          : <h1 className='text-white text-3xl font-bold leading-10'>Chat Time</h1>
+        {
+          loading || !user
+          ? <h1 className='text-white text-3xl font-bold leading-10'>Chat Time</h1>
+          : <Link href='/' className='text-white text-3xl font-bold leading-10 duration-300 hover:opacity-60'>Chat Time</Link>
         }
         {user && <LogoutButton />}
       </div>
