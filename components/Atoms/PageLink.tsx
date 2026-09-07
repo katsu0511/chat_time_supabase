4ffb0cefed6816184eb043077a091bc6e4704406
Katsuya <haradakatsuya190511@gmail.com>
2026-09-07 15:46:31 +0700

    modified PageLink

--- components/Atoms/PageLink.tsx
+++ components/Atoms/PageLink.tsx
@@ -1,12 +1,19 @@
+'use client';
+
+import useLoading from '@/lib/hooks/useLoading';
 import Link from 'next/link';
 
-export default function PageLink({ path, display, disabled = false }: { path: string, display: string, disabled?: boolean }) {
+export default function PageLink({ path, display }: { path: string, display: string }) {
+  const { loading, setLoading } = useLoading();
+
+  const commonClassName = 'inline-block font-bold duration-300';
+
   return (
     <div className='flex justify-center mt-10'>
       {
-        disabled
-        ? <span className='inline-block text-gray-400 font-bold duration-300 cursor-not-allowed'>&gt;&gt; {display}</span>
-        : <Link href={`./${path}`} className='inline-block text-[color:var(--color-primary)] font-bold duration-300 cursor-pointer hover:opacity-40'>&gt;&gt; {display}</Link>
+        loading
+        ? <span className={`${commonClassName} text-gray-400 cursor-not-allowed`}>&gt;&gt; {display}</span>
+        : <Link href={`./${path}`} className={`${commonClassName} text-[color:var(--color-primary)] cursor-pointer hover:opacity-40`} onClick={() => setLoading(true)}>&gt;&gt; {display}</Link>
       }
     </div>
   );
