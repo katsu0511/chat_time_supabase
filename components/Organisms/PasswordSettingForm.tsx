bceb1ace9c34b64133fdaf261dc152803f5f1dba
Katsuya <haradakatsuya190511@gmail.com>
2026-09-07 18:16:52 +0700

    added setLoading

--- components/Organisms/PasswordSettingForm.tsx
+++ components/Organisms/PasswordSettingForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import useLoading from '@/lib/hooks/useLoading';
 import useAuth from '@/lib/hooks/useAuth';
 import { checkPassword, handleLogout } from '@/lib/api/auth';
@@ -87,6 +87,10 @@ export default function PasswordSettingForm({ user }: { user: AppUser }) {
     setPendingData(null);
     setLoading(false);
   };
+
+  useEffect(() => {
+    setLoading(false);
+  }, [setLoading]);
 
   return (
     <div className='flex items-center w-full h-full'>
