a1996d876a795d3c9a636d89462dec509ec905cf
Katsuya <haradakatsuya190511@gmail.com>
2026-09-08 19:54:19 +0700

    added setLoading

--- components/Organisms/Header.tsx
+++ components/Organisms/Header.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import LogoutButton from '@/components/Atoms/LogoutButton';
 
 export default function Header({ user }: { user: AuthUser | null }) {
-  const { loading } = useLoading();
+  const { loading, setLoading } = useLoading();
 
   return (
     <header className='fixed bg-[color:var(--color-primary)] w-full h-10 z-20'>
@@ -14,7 +14,7 @@ export default function Header({ user }: { user: AuthUser | null }) {
         {
           loading || !user
           ? <h1 className='text-white text-3xl font-bold leading-10'>Chat Time</h1>
-          : <Link href='/' className='text-white text-3xl font-bold leading-10 duration-300 hover:opacity-60'>Chat Time</Link>
+          : <Link href='/' className='text-white text-3xl font-bold leading-10 duration-300 hover:opacity-60' onClick={() => setLoading(true)}>Chat Time</Link>
         }
         {user && <LogoutButton />}
       </div>
