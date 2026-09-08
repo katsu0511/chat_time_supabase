0336d5d07bfe834ae099f89d06211cd6497dd2c5
Katsuya <haradakatsuya190511@gmail.com>
2026-09-08 20:35:10 +0700

    added sendingState and setSendingState

--- components/Templates/LoadingProviderWrapper.tsx
+++ components/Templates/LoadingProviderWrapper.tsx
@@ -5,15 +5,18 @@ import { Dispatch, SetStateAction, createContext, useState } from 'react';
 type LoadingContext = {
   loading: boolean
   setLoading: Dispatch<SetStateAction<boolean>>
+  sendingState: boolean
+  setSendingState: Dispatch<SetStateAction<boolean>>
 };
 
 export const LoadingContext = createContext<LoadingContext | undefined>(undefined);
 
 export function LoadingProviderWrapper({ children }: { children: React.ReactNode }) {
   const [loading, setLoading] = useState<boolean>(false);
+  const [sendingState, setSendingState] = useState<boolean>(false);
 
   return (
-    <LoadingContext.Provider value={{ loading, setLoading }}>
+    <LoadingContext.Provider value={{ loading, setLoading, sendingState, setSendingState }}>
       {children}
     </LoadingContext.Provider>
   );
