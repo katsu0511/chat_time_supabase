ed1f49549f6e5d618305a1580ba5e900e01c19a5
Katsuya <haradakatsuya190511@gmail.com>
2026-09-08 22:45:00 +0700

    deleted disabled props

--- components/Molecules/Input.tsx
+++ components/Molecules/Input.tsx
@@ -1,18 +1,27 @@
 import { ChangeEvent } from 'react';
+import useLoading from '@/lib/hooks/useLoading';
 
-export default function Input(props: {label: string, type: string, value: string, disabled: boolean, onChange: (e: ChangeEvent<HTMLInputElement>) => void}) {
-  const inputClassName = props.disabled ? 'border-gray-400 cursor-not-allowed' : 'border-[color:var(--color-primary)] cursor-text';
+type Props = {
+  label: string
+  type: string
+  value: string
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+};
+
+export default function Input({ label, type, value, onChange }: Props) {
+  const { loading } = useLoading();
+  const inputClassName = loading ? 'border-gray-400 cursor-not-allowed' : 'border-[color:var(--color-primary)] cursor-text';
 
   return (
     <div className='flex justify-center items-center py-2'>
-      <label htmlFor={props.label} className='block w-20'>{props.label}: </label>
+      <label htmlFor={label} className='block w-20'>{label}: </label>
       <input
-        id={props.label}
-        type={props.type}
-        disabled={props.disabled}
+        id={label}
+        type={type}
+        disabled={loading}
         className={`w-47 h-9 border-2 px-2 py-1 outline-none appearance-none ${inputClassName}`}
-        value={props.value}
-        onChange={props.onChange}
+        value={value}
+        onChange={onChange}
         required
       />
     </div>
