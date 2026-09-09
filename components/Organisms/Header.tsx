09a30bd0cd3976c05429633dc2c6a2a84528cac1
Katsuya <haradakatsuya190511@gmail.com>
2026-09-09 17:59:59 +0700

    resolved loading bug

--- components/Organisms/Header.tsx
+++ components/Organisms/Header.tsx
@@ -2,11 +2,19 @@
 
 import type { User as AuthUser } from '@supabase/supabase-js';
 import useLoading from '@/lib/hooks/useLoading';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import LogoutButton from '@/components/Atoms/LogoutButton';
 
 export default function Header({ user }: { user: AuthUser | null }) {
   const { loading, setLoading } = useLoading();
+  const pathname = usePathname();
+
+  const onMoveToHome = () => {
+    if (pathname !== '/') {
+      setLoading(true);
+    }
+  };
 
   return (
     <header className='fixed bg-[color:var(--color-primary)] w-full h-10 z-20'>
@@ -14,7 +22,7 @@ export default function Header({ user }: { user: AuthUser | null }) {
         {
           loading || !user
           ? <h1 className='text-white text-3xl font-bold leading-10'>Chat Time</h1>
-          : <Link href='/' className='text-white text-3xl font-bold leading-10 duration-300 hover:opacity-60' onClick={() => setLoading(true)}>Chat Time</Link>
+          : <Link href='/' className='text-white text-3xl font-bold leading-10 duration-300 hover:opacity-60' onClick={onMoveToHome}>Chat Time</Link>
         }
         {user && <LogoutButton />}
       </div>
