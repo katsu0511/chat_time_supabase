b652a9e390aba03647555ca2f04737f986f69b68
Katsuya <haradakatsuya190511@gmail.com>
2026-09-09 16:53:37 +0700

    added some state to LoadingProviderWrapper.tsx

--- components/Templates/LoadingProviderWrapper.tsx
+++ components/Templates/LoadingProviderWrapper.tsx
@@ -7,6 +7,10 @@ type LoadingContext = {
   setLoading: Dispatch<SetStateAction<boolean>>
   sendingState: boolean
   setSendingState: Dispatch<SetStateAction<boolean>>
+  errorMessage: string
+  setErrorMessage: Dispatch<SetStateAction<string>>
+  displayErrorModal: boolean
+  setDisplayErrorModal: Dispatch<SetStateAction<boolean>>
 };
 
 export const LoadingContext = createContext<LoadingContext | undefined>(undefined);
@@ -14,9 +18,11 @@ export const LoadingContext = createContext<LoadingContext | undefined>(undefine
 export function LoadingProviderWrapper({ children }: { children: React.ReactNode }) {
   const [loading, setLoading] = useState<boolean>(false);
   const [sendingState, setSendingState] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [displayErrorModal, setDisplayErrorModal] = useState<boolean>(false);
 
   return (
-    <LoadingContext.Provider value={{ loading, setLoading, sendingState, setSendingState }}>
+    <LoadingContext.Provider value={{ loading, setLoading, sendingState, setSendingState, errorMessage, setErrorMessage, displayErrorModal, setDisplayErrorModal }}>
       {children}
     </LoadingContext.Provider>
   );
