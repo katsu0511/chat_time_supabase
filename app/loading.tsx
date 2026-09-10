977d74ed905a1ade62811cb45a45ef5d3bb26bf7
Katsuya <haradakatsuya190511@gmail.com>
2026-09-10 16:45:38 +0700

    updated loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,18 @@ import { Backdrop, CircularProgress } from '@mui/material';
 
 export default function Loading() {
   return (
-    <Backdrop open>
+    <Backdrop
+      open
+      sx={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        zIndex: 100
+      }}
+    >
       <CircularProgress color='inherit' />
+      <p className='text-[color:var(--text-primary)] text-2xl ml-5 animate-pulse md:text-5xl'>Loading...</p>
     </Backdrop>
   );
 }
